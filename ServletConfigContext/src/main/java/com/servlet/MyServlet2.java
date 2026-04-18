7014697793f3e162d70159814244305727f1eb80
hetsikhunt12 <hetsikhunt@gmail.com>
2026-04-18 17:24:18 +0530

    servlet

---
+++ ServletConfigContext/src/main/java/com/servlet/MyServlet2.java
@@ -0,0 +1,44 @@
+package com.servlet;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/MyServlet2")
+public class MyServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	ServletConfig config;
+	ServletContext context;
+	String email,driver;
+	
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init called");
+		this.config=config;
+		this.context=config.getServletContext();
+	}
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet");
+		email = config.getInitParameter("email");
+		System.out.println("Email : " + email);
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.print("Email : " + email);
+		driver = context.getInitParameter("driver");
+		System.out.println("Driver : " + driver);
+		out.print("Driver : " + driver);
+	}
+
+	public void destroy() {
+		System.out.println("Destroy called");
+	}
+
+}
---
+++ WebApplicationListnerDemo/src/main/java/com/listener/MyListener.java
@@ -0,0 +1,19 @@
+package com.listener;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+@WebListener
+public class MyListener implements ServletContextListener {
+
+
+    public void contextInitialized(ServletContextEvent sce)  { 
+    	sce.getServletContext().getAttribute("counter");
+    }
+    
+    public void contextDestroyed(ServletContextEvent sce)  { 
+    	sce.getServletContext().getAttribute("counter");
+    }
+	
+}
